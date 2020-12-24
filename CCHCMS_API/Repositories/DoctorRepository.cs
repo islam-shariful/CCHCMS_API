2b01e8a7458381f8e8375798301c1cea7f71466b
Md. Shariful Islam <imdshariful171@gmail.com>
2020-12-25 02:07:26 +0600

    Repositories Added

---
+++ CCHCMS_API/Repositories/DoctorRepository.cs
@@ -0,0 +1,13 @@
+﻿using CCHCMS_API.Repositories;
+using CCHCMS_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CCHCMS_API.Repositories
+{
+    public class PostRepository : Repository<Doctor>
+    {
+    }
+}
