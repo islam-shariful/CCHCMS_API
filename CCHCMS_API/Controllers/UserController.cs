1edc84f38f6663c804d8f917ba8051d11989c4b4
Md. Shariful Islam <imdshariful171@gmail.com>
2020-12-25 21:46:15 +0600

    User's API Added

---
+++ CCHCMS_API/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using CCHCMS_API.Models;
+using CCHCMS_API.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CCHCMS_API.Controllers
+{
+    [RoutePrefix("api/users")]
+    public class UserController : ApiController
+    {
+        UserRepository userRepository = new UserRepository();
+        [Route("", Name = "GetUsers")]
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            var users = userRepository.GetAll();
+            if (users == null)
+            {
+                return StatusCode(HttpStatusCode.NotFound);
+            }
+            else
+                return Ok(users);
+        }
+        [Route("{id}", Name = "GetUser")]
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            var user = userRepository.Get(id);
+            if (user == null)
+            {
+                return StatusCode(HttpStatusCode.NotFound);
+            }
+            else
+                return Ok(user);
+        }
+        [Route("", Name = "PostUser")]
+        [HttpPost]
+        public IHttpActionResult Post(User user)
+        {
+            userRepository.Insert(user);
+            string uri = Url.Link("GetUser", new { id = user.Id });
+            return Created(uri, user);
+        }
+        [Route("{id}", Name = "UpdateUser")]
+        [HttpPut]
+        public IHttpActionResult Put([FromUri] int id, [FromBody] User user)
+        {
+            user.Id = id;
+            userRepository.Update(user);
+            return Ok(user);
+        }
+        [Route("{id}", Name = "DeleteUser")]
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            userRepository.Delete(id);
+            return StatusCode(HttpStatusCode.NotFound);
+        }
+    }
+}
---
+++ CCHCMS_API/Models/PartialUser.cs
@@ -0,0 +1,30 @@
+﻿using Social_API.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CCHCMS_API.Models
+{
+    [MetadataType(typeof(UserMetadata))]
+    public partial class User
+    {
+        public List<Link> Links { get { return GeneralLinks(); } }
+        private List<Link> GeneralLinks()
+        {
+            List<Link> Links = new List<Link>();
+            HttpContext context = HttpContext.Current;
+            string baseUrl = context.Request.Url.Scheme + "://" + context.Request.Url.Authority + context.Request.ApplicationPath.TrimEnd('/') + '/';
+            string usersUrl = "api/users/";
+
+            Links.Add(new Link() { Url = baseUrl + usersUrl, Method = "Get", Relation = "GetUsers" });
+            Links.Add(new Link() { Url = baseUrl + usersUrl + this.Id, Method = "Get", Relation = "GetUser" });
+            Links.Add(new Link() { Url = baseUrl + usersUrl, Method = "Post", Relation = "AddUser" });
+            Links.Add(new Link() { Url = baseUrl + usersUrl + this.Id, Method = "Put", Relation = "UpdateUser" });
+            Links.Add(new Link() { Url = baseUrl + usersUrl + this.Id, Method = "Delete", Relation = "DeleteUser" });
+
+            return Links;
+        }
+    }
+}
