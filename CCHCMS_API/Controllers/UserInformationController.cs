a6323b23015d9feb0fbc8fe33a4ea8eceb5a96b2
Md. Shariful Islam <imdshariful171@gmail.com>
2020-12-25 22:00:12 +0600

    UserInformation's API Added

---
+++ CCHCMS_API/Controllers/UserInformationController.cs
@@ -0,0 +1,63 @@
+﻿using CCHCMS_API.Models;
+using CCHCMS_API.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CCHCMS_API.Controllers
+{
+    [RoutePrefix("api/userinformations")]
+    public class UserInformationController : ApiController
+    {
+        UserInformationRepository userInformationRepository = new UserInformationRepository();
+        [Route("", Name = "GetUserInformations")]
+        [HttpGet]
+        public IHttpActionResult Get()
+
+        {
+            var userInformations = userInformationRepository.GetAll();
+            if (userInformations == null)
+            {
+                return StatusCode(HttpStatusCode.NotFound);
+            }
+            return Ok(userInformations);
+        }
+        [Route("{id}", Name = "GetUserInformation")]
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            var userInformations = userInformationRepository.Get(id);
+            if (userInformations == null)
+            {
+                return StatusCode(HttpStatusCode.NotFound);
+            }
+            return Ok(userInformations);
+        }
+        [Route("", Name = "PostUserInformation")]
+        [HttpPost]
+        public IHttpActionResult Post(UserInformation userInformation)
+        {
+            userInformationRepository.Insert(userInformation);
+            string uri = Url.Link("GetUserInformation", new { id = userInformation.Id });
+            return Created(uri, userInformation);
+        }
+        [Route("{id}", Name = "UpdateUserInformation")]
+        [HttpPut]
+        public IHttpActionResult Put([FromUri] int id, [FromBody] UserInformation userinformation)
+        {
+            userinformation.Id = id;
+            userInformationRepository.Update(userinformation);
+            return Ok(userinformation);
+        }
+        [Route("{id}", Name = "DeleteUserInformation")]
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            userInformationRepository.Delete(id);
+            return StatusCode(HttpStatusCode.NotFound);
+        }
+    }
+}
---
+++ CCHCMS_API/Models/PartialUserInformation.cs
@@ -0,0 +1,28 @@
+﻿using Social_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CCHCMS_API.Models
+{
+    public partial class UserInformation
+    {
+        public List<Link> Links { get { return GeneralLinks(); } }
+        private List<Link> GeneralLinks()
+        {
+            List<Link> Links = new List<Link>();
+            HttpContext context = HttpContext.Current;
+            string baseUrl = context.Request.Url.Scheme + "://" + context.Request.Url.Authority + context.Request.ApplicationPath.TrimEnd('/') + '/';
+            string userInformationsUrl = "api/userinformations/";
+
+            Links.Add(new Link() { Url = baseUrl + userInformationsUrl, Method = "Get", Relation = "GetUserInformations" });
+            Links.Add(new Link() { Url = baseUrl + userInformationsUrl + this.Id, Method = "Get", Relation = "GetUserInformation" });
+            Links.Add(new Link() { Url = baseUrl + userInformationsUrl, Method = "Post", Relation = "AddUserInformation" });
+            Links.Add(new Link() { Url = baseUrl + userInformationsUrl + this.Id, Method = "Put", Relation = "UpdateUserInformation" });
+            Links.Add(new Link() { Url = baseUrl + userInformationsUrl + this.Id, Method = "Delete", Relation = "DeleteUserInformation" });
+
+            return Links;
+        }
+    }
+}
