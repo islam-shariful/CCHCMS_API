8ad9d1c84adebc6e7d91f03db8073833e55c5c57
Md. Shariful Islam <imdshariful171@gmail.com>
2020-12-25 20:32:31 +0600

    Prescription's API Added

---
+++ CCHCMS_API/Controllers/PrescriptionController.cs
@@ -0,0 +1,63 @@
+﻿using CCHCMS_API.Models;
+using CCHCMS_API.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CCHCMS_API.Controllers
+{
+    [RoutePrefix("api/prescriptions")]
+    public class PrescriptionController : ApiController
+    {
+        PrescriptionRepository prescriptionRepository = new PrescriptionRepository();
+        [Route("", Name = "GetPrescriptions")]
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            var prescriptions = prescriptionRepository.GetAll();
+            if (prescriptions == null)
+            {
+                return StatusCode(HttpStatusCode.NotFound);
+            }
+            else
+                return Ok(prescriptions);
+        }
+        [Route("{id}", Name = "GetPrescription")]
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            var prescription = prescriptionRepository.Get(id);
+            if (prescription == null)
+            {
+                return StatusCode(HttpStatusCode.NotFound);
+            }
+            return Ok(prescription);
+        }
+        [Route("", Name = "PostPrescription")]
+        [HttpPost]
+        public IHttpActionResult Post(Prescription prescription)
+        {
+            prescriptionRepository.Insert(prescription);
+            string uri = Url.Link("GetPrescription", new { id = prescription.Id });
+            return Created(uri, prescription);
+        }
+        [Route("{id}", Name = "UpdatePrescription")]
+        [HttpPut]
+        public IHttpActionResult Put([FromUri] int id, [FromBody] Prescription prescription)
+        {
+            prescription.Id = id;
+            prescriptionRepository.Update(prescription);
+            return Ok(prescription);
+        }
+        [Route("{id}", Name = "DeletePrescription")]
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            prescriptionRepository.Delete(id);
+            return StatusCode(HttpStatusCode.NotFound);
+        }
+    }
+}
---
+++ CCHCMS_API/Models/PartialPrescription.cs
@@ -0,0 +1,28 @@
+﻿using Social_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CCHCMS_API.Models
+{
+    public partial class Prescription
+    {
+        public List<Link> Links { get { return GeneralLinks(); } }
+        private List<Link> GeneralLinks()
+        {
+            List<Link> Links = new List<Link>();
+            HttpContext context = HttpContext.Current;
+            string baseUrl = context.Request.Url.Scheme + "://" + context.Request.Url.Authority + context.Request.ApplicationPath.TrimEnd('/') + '/';
+            string prescriptionUrl = "api/prescriptions/";
+
+            Links.Add(new Link() { Url = baseUrl + prescriptionUrl, Method = "Get", Relation = "GetPrescriptions" });
+            Links.Add(new Link() { Url = baseUrl + prescriptionUrl + this.Id, Method = "Get", Relation = "GetPrescription" });
+            Links.Add(new Link() { Url = baseUrl + prescriptionUrl, Method = "Post", Relation = "AddPrescription" });
+            Links.Add(new Link() { Url = baseUrl + prescriptionUrl + this.Id, Method = "Put", Relation = "UpdatePrescription" });
+            Links.Add(new Link() { Url = baseUrl + prescriptionUrl + this.Id, Method = "Delete", Relation = "DeletePrescription" });
+
+            return Links;
+        }
+    }
+}
