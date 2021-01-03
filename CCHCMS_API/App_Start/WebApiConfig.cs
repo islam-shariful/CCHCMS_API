bc67034a88eaaec215fb74662802708115f34b18
Md. Shariful Islam <imdshariful171@gmail.com>
2021-01-03 17:24:16 +0600

    Cors Impl.

--- CCHCMS_API/App_Start/WebApiConfig.cs
+++ CCHCMS_API/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace CCHCMS_API
 {
@@ -19,6 +20,9 @@ namespace CCHCMS_API
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
         }
     }
 }
